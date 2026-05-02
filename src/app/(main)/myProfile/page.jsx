df8006aa342300ed081e764eee48cc312f1fd4a9
Armana Anie <viviandesena96@gmail.com>
2026-05-02 13:11:11 +0600

    moving to deploy

--- src/app/(main)/myProfile/page.jsx
+++ src/app/(main)/myProfile/page.jsx
@@ -1,15 +1,21 @@
 import ProfileHeader from '@/components/shared/ProfileHeader';
+import { auth } from '@/lib/auth';
+import { headers } from 'next/headers';
 import Image from 'next/image';
 
 
-const MyProfilePage = () => {
+const MyProfilePage = async() => {
+    const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  const user = session?.user;
     return (
         <div>
            <ProfileHeader></ProfileHeader>
-           <div className='flex flex-col items-center justify-center gap-4 shadow-sm p-10 my-10'><Image src="https://plus.unsplash.com/premium_photo-1673367754031-65483137554c?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt='user pic' className='rounded-full bg-green-600 p-1' height={100} width={100}></Image>
-           <span className='font-bold text-xl'>User Name</span>
-           <p>Email:<span className='font-bold'>@.com</span></p>
-           <p>Phone:<span className='font-bold'>1234567890</span></p></div>
+           <div className='flex flex-col items-center justify-center gap-4 shadow-sm p-10 my-10'><Image src={user?.image || "https://images.unsplash.com/photo-1567446537708-ac4aa75c9c28?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}alt='user pic' className='rounded-full bg-green-600 p-1' height={100} width={100}></Image>
+           <span className='font-bold text-xl'>{user?.name}</span>
+           <p>Email:<span className='font-bold'>{user?.email}</span></p>
+           <p>Phone<span className='text-[10px]'>(dummy)</span>:<span className='font-bold'>1234567890</span></p></div>
 
         </div>
     );
--- src/app/(main)/updateProfile/page.jsx
+++ src/app/(main)/updateProfile/page.jsx
@@ -1,21 +1,44 @@
+"use client"
 import ProfileHeader from '@/components/shared/ProfileHeader';
-import React from 'react';
+import { authClient } from '@/lib/auth-client';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
 
 const UpdateUserPage = () => {
+    const router =useRouter();
+    const [name,setName]= useState("");
+    const [image,setImage]= useState("");
+    const handleUpdate = async (e)=>{
+        e.preventDefault();
+
+        try{
+            await authClient.updateUser({
+                name,image,
+            });
+            toast.success("Profile updated successfully");
+            router.push('/myProfile');
+        } catch (error){
+            toast.error("Update failed");
+            console.log(error)
+        }
+    }
     return (<div className='my-10'>
         <ProfileHeader></ProfileHeader>
         <h1 className='font-bold text-3xl text-green-600 text-center p-4'>Update Profile</h1>
-        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs mx-auto border p-4">
+
+        <form onSubmit={handleUpdate}> <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs mx-auto border p-4">
 
 
   <label className="label text-green-600">Image</label>
- <input type="text" placeholder="Image Url" className="input input-success" />
+ <input type="text" value={image}placeholder="Image Url" className="input input-success" onChange={(e)=>setImage(e.target.value)} />
 
   <label className="label text-green-600">Name</label>
-  <input type="text" placeholder="Name" className="input input-success" />
+  <input type="text" placeholder="Name" className="input input-success" value={name} onChange={(e)=>setName(e.target.value)} />
 
-  <button className="btn btn-success mt-4 text-white">Update Info</button>
-</fieldset>
+  <button className="btn btn-success mt-4 text-white" type='submit'>Update Info</button>
+</fieldset></form>
+       
 </div>
     );
 };
